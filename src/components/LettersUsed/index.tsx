d0be9eb5b71789cd2a4d467fb10fa10b3edbe8d4
Raphael-Steimvacher <raphaelSteimvacher2251@gmail.com>
2025-10-31 20:03:03 -0300

    criando o componente LettersUsed e estilizando

---
+++ src/components/LettersUsed/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.css"
+
+type Props = {
+
+}
+
+export function LettersUsed({}:Props){
+  return(
+<div className={styles.lettersUsed}>
+  <h5>Letras Utilizadas</h5>
+</div>
+  )
+}
