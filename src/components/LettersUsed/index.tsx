87fc7fa1a4374d1d5e670811906db8e2686de6eb
Raphael-Steimvacher <raphaelSteimvacher2251@gmail.com>
2025-10-31 21:44:28 -0300

    aplicando variações de cores

--- src/components/LettersUsed/index.tsx
+++ src/components/LettersUsed/index.tsx
@@ -9,8 +9,8 @@ export function LettersUsed() {
     <div className={styles.lettersUsed}>
       <h5>Letras Utilizadas</h5>
       <div>
-        <Letter value="X" size="small" />
-        <Letter value="R" size="small"/>
+        <Letter value="X" size="small" color="correct" />
+        <Letter value="R" size="small" color="wrong" />
       </div>
     </div>
   )
